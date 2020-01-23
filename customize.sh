8b63f98b25f0fa86f07e41c55eb227f96f71014a
3arthur6 <arthur.trouillot@gmail.com>
2020-01-23 18:03:09 +0100

    Safer model & system path detection

--- customize.sh
+++ customize.sh
@@ -2,31 +2,23 @@
 # by 3arthur6
 
 set_vars() {
-  if [ "$BOOTMODE" == "true" ] ; then
-    ui_print "- Magisk Manager installation"
-    sys_path="/sbin/.magisk/mirror/system"
-    manufacturer=$(getprop ro.product.manufacturer)
-    model=$(getprop ro.product.model)
-  else
-    ui_print "- Recovery installation"
-    if [ -d /system_root ] ; then
-      sys_path="/system_root/system"
-    elif [ -d /system/system ] ; then
-      sys_path="/system/system"
-    else
-      sys_path="/system"
-    fi
-    manufacturer=$(grep .manufacturer= $sys_path/build.prop | cut -d "=" -f2)
-    model=$(grep .model= $sys_path/build.prop | cut -d "=" -f2)
-  fi
-  if [ ! "$manufacturer" == "samsung" ] ; then
+  model=`grep -o androidboot.em.model=........ /proc/cmdline | cut -d "=" -f2`
+
+  if [ -z $model ] ; then
     abort "- Only for Samsung devices!"
   fi
+  if $BOOTMODE ; then
+    ui_print "- Magisk Manager installation"
+    sys_path="/sbin/.magisk/mirror/system"
+  else
+    ui_print "- Recovery installation"
+    sys_path=`find $ANDROID_ROOT -mindepth 1 -maxdepth 2 -path "*system/build.prop" | xargs dirname`
+  fi
   if [ $API == 29 ] ; then
-    ui_print "- Android 10 detected"
+    ui_print "- $model on Android 10 detected"
     library="libbluetooth.so"
-    path="$MODPATH/system/lib64/libbluetooth.so"
-    if [ "$model" == "SM-G9700" ] || [ "$model" == "SM-G9730" ] || [ "$model" == "SM-G9750" ] || [ "$model" == "SM-N9700" ] || [ "$model" == "SM-N9750" ] || [ "$model" == "SM-N9760" ] ; then
+    path="$MODPATH/system/lib64/$library"
+    if echo $model | grep -Eq SM-[GN]97[0356]0 ; then
       pre_hex="88000054691180522925C81A69000037E00300326D020014"
       post_hex="04000014691180522925C81A1F2003D5E0031F2A6D020014"
     else
@@ -34,25 +26,25 @@ set_vars() {
       post_hex="1F2003D5F4031F2AF3031F2AE8031F2A"
     fi
   elif [ $API == 28 ] ; then
-    ui_print "- Android Pie detected"
+    ui_print "- $model on Android Pie detected"
     library="libbluetooth.so"
-    path="$MODPATH/system/lib64/libbluetooth.so"
+    path="$MODPATH/system/lib64/$library"
     pre_hex="88000034E803003248070035"
     post_hex="1F2003D5E8031F2A48070035"
   elif [ $API == 27 ] ; then
-    ui_print "- Android Oreo 8.1 detected"
+    ui_print "- $model on Android Oreo 8.1 detected"
     library="bluetooth.default.so"
-    path="$MODPATH/system/lib64/hw/bluetooth.default.so"
-    path2="$MODPATH/system/lib/hw/bluetooth.default.so"
+    path="$MODPATH/system/lib64/hw/$library"
+    path2="$MODPATH/system/lib/hw/$library"
     pre_hex="88000034E803003228050035"
     pre_hex2="0978019009B1012032E07748"
     post_hex="1F2003D5E8031F2A28050035"
     post_hex2="0978019000BF002032E07748"
   elif [ $API == 26 ] ; then
-    ui_print "- Android Oreo 8.0 detected"
+    ui_print "- $model on Android Oreo 8.0 detected"
     library="bluetooth.default.so"
-    path="$MODPATH/system/lib64/hw/bluetooth.default.so"
-    path2="$MODPATH/system/lib/hw/bluetooth.default.so"
+    path="$MODPATH/system/lib64/hw/$library"
+    path2="$MODPATH/system/lib/hw/$library"
     pre_hex="88000034E803003228050035"
     pre_hex2="0190087808B1012031E07548"
     post_hex="1F2003D5E8031F2A28050035"
@@ -65,7 +57,7 @@ set_vars() {
 check_lib() {
   if [ $API -ge 28 ] && [ ! -f "$sys_path/lib64/$library" ] ; then
     abort "- No $library library found!"
-  elif [ $API -le 27 ] && [ [ ! -f "$sys_path/lib64/hw/$library" ] || [ ! -f "$sys_path/lib64/hw/$library" ] ] ; then
+  elif [ $API -le 27 ] && [ [ ! -f "$sys_path/lib64/hw/$library" ] || [ ! -f "$sys_path/lib/hw/$library" ] ] ; then
     abort "- No $library libraries found!"
   fi
 }
@@ -73,11 +65,11 @@ check_lib() {
 extract() {
   if [ $API -ge 28 ] ; then
     mkdir -p $MODPATH/system/lib64
-    ui_print "- Extracting $library library from system"
+    ui_print "- Copying library from system to module"
     cp -af $sys_path/lib64/$library $path
   else
     mkdir -p $MODPATH/system/lib64/hw $MODPATH/system/lib/hw
-    ui_print "- Extracting $library libraries from system"
+    ui_print "- Copying libraries from system to module"
     cp -af $sys_path/lib64/hw/$library $path
     cp -af $sys_path/lib/hw/$library $path2
   fi
