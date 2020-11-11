80832b47f7e76d20caa8e4c325f953f78a799175
3arthur6 <arthur.trouillot@gmail.com>
2020-11-11 15:10:14 +0100

    Large rewrite
    Cleanup && include bash for array handling

--- customize.sh
+++ customize.sh
@@ -1,171 +1,73 @@
 # BluetoothLibraryPatcher
 # by 3arthur6
 
-set_vars() {
+check() {
   samsung=`grep -Eqw "androidboot.odin_download|androidboot.warranty_bit|sec_debug" /proc/cmdline && echo 'true' || echo 'false'`
-  bootloader=`grep -o androidboot.bootloader=.* /proc/cmdline | cut -d ' ' -f1 | cut -d '=' -f2`
-  qcom=`grep -qw androidboot.hardware=qcom /proc/cmdline && echo 'true' || echo 'false'`
-
   if $BOOTMODE ; then
     ui_print "- Magisk Manager installation"
     sys="`magisk --path`/.magisk/mirror/system"
   else
     ui_print "- Recovery installation"
-    sys=`find $ANDROID_ROOT -mindepth 1 -maxdepth 2 -path "*system/build.prop" | xargs dirname`
+    sys=`dirname $(find $ANDROID_ROOT -mindepth 1 -maxdepth 2 -path "*system/build.prop")`
   fi
   if ! $samsung ; then
     ui_print "- Only for Samsung devices!"
     abort
-  fi
-  if ! grep -qw ro.build.type=user $sys/build.prop ; then
+  elif ! `grep -qw ro.build.type=user $sys/build.prop` ; then
     ui_print "- Only for Samsung stock based roms!"
     ui_print "- Not relevant for aosp roms!"
     abort
-  fi
-  if [ ${#bootloader} == 12 ] ; then
-    model=SM-${bootloader:0:4}
-  elif [ ${#bootloader} == 13 ] ; then
-    model=SM-${bootloader:0:5}
-  elif [ ${#bootloader} == 14 ] ; then
-    model=SM-${bootloader:0:6}
-  else
-    model="Model not found"
-  fi
-  ui_print "- Searching for the hex sequence"
-  if [ $API == 30 ] ; then
-    ui_print "- $model on Android 11 detected"
-    mod_path="$MODPATH/system/lib64/libbluetooth.so"
-    sys_path="$sys/lib64/libbluetooth.so"
-    if $qcom && xxd -p $sys_path | tr -d '\n' | grep -iq 88000054691180522925C81A6900003720008052 ; then
-      pre_hex="88000054691180522925C81A6900003720008052"
-      post_hex="04000014691180522925C81A69000037E0031F2A"
-    else
-      pre_hex=`xxd -p $sys_path | tr -d '\n' | grep -io ........F3031F2AF4031F2A3E000014 | tr '[:lower:]' '[:upper:]'`
-      if [ -z $pre_hex ] ; then
-        pre_hex="not_found"
-      fi
-      post_hex="1F2003D5F3031F2AF4031F2A3E000014"
-    fi
-  elif [ $API == 29 ] ; then
-    ui_print "- $model on Android 10 detected"
-    mod_path="$MODPATH/system/lib64/libbluetooth.so"
-    sys_path="$sys/lib64/libbluetooth.so"
-    if ! $IS64BIT ; then
-      mod_path=`echo $mod_path | tr -d '64'`
-      sys_path=`echo $sys_path | tr -d '64'`
-      pre_hex=`xxd -p $sys_path | tr -d '\n' | grep -io ..B100250120 | tr '[:lower:]' '[:upper:]'`
-      if [ -z $pre_hex ] ; then
-        pre_hex="not_found"
-      fi
-      post_hex="00BF00250020"
-    elif $qcom && xxd -p $sys_path | tr -d '\n' | grep -iq 88000054691180522925C81A69000037E0030032 ; then
-      pre_hex="88000054691180522925C81A69000037E0030032"
-      post_hex="04000014691180522925C81A69000037E0031F2A"
-    else
-      pre_hex=`xxd -p $sys_path | tr -d '\n' | grep -io ........F4031F2AF3031F2AE8030032 | tr '[:lower:]' '[:upper:]'`
-      if [ -z $pre_hex ] ; then
-        pre_hex="not_found"
-      fi
-      post_hex="1F2003D5F4031F2AF3031F2AE8031F2A"
-    fi
-  elif [ $API == 28 ] ; then
-    ui_print "- $model on Android Pie detected"
-    mod_path="$MODPATH/system/lib64/libbluetooth.so"
-    sys_path="$sys/lib64/libbluetooth.so"
-    if ! $IS64BIT ; then
-      mod_path=`echo $mod_path | tr -d '64'`
-      sys_path=`echo $sys_path | tr -d '64'`
-      pre_hex=`xxd -p $sys_path | tr -d '\n' | grep -io ..B101200028 | tr '[:lower:]' '[:upper:]'`
-      if [ -z $pre_hex ] ; then
-        pre_hex="not_found"
-      fi
-      post_hex="00BF00200028"
-    elif $qcom && xxd -p $sys_path | tr -d '\n' | grep -iq 7F1D0071E91700F9E83C0054 ; then
-      pre_hex="7F1D0071E91700F9E83C0054"
-      post_hex="E0031F2AE91700F9E8010014"
-    else
-      pre_hex="88000034E803003248070035"
-      post_hex="1F2003D5E8031F2A48070035"
-    fi
-  elif [ $API == 27 ] ; then
-    ui_print "- $model on Android Oreo 8.1 detected"
-    mod_path="$MODPATH/system/lib64/hw/bluetooth.default.so"
-    sys_path="$sys/lib64/hw/bluetooth.default.so"
-    if ! $IS64BIT ; then
-      mod_path=`echo $mod_path | tr -d '64'`
-      sys_path=`echo $sys_path | tr -d '64'`
-      pre_hex="09B1012032E0"
-      post_hex="00BF002032E0"
-    else
-      pre_hex="88000034E803003228050035"
-      post_hex="1F2003D5E8031F2A28050035"
-    fi
-  elif [ $API == 26 ] ; then
-    ui_print "- $model on Android Oreo 8.0 detected"
-    mod_path="$MODPATH/system/lib64/hw/bluetooth.default.so"
-    sys_path="$sys/lib64/hw/bluetooth.default.so"
-    if ! $IS64BIT ; then
-      mod_path=`echo $mod_path | tr -d '64'`
-      sys_path=`echo $sys_path | tr -d '64'`
-      pre_hex="08B1012031E0"
-      post_hex="00BF002031E0"
-    else
-      pre_hex="88000034E803003228050035"
-      post_hex="1F2003D5E8031F2A28050035"
-    fi
-  elif [ $API == 25 ] ; then
-    ui_print "- $model on Android Nougat 7.1 detected"
-    mod_path="$MODPATH/system/lib/hw/bluetooth.default.so"
-    sys_path="$sys/lib/hw/bluetooth.default.so"
-    pre_hex="087850BBB548"
-    post_hex="0878BDE0B548"
-  elif [ $API == 24 ] ; then
-    ui_print "- $model on Android Nougat 7.0 detected"
-    mod_path="$MODPATH/system/lib/hw/bluetooth.default.so"
-    sys_path="$sys/lib/hw/bluetooth.default.so"
-    pre_hex="007840BB6A48"
-    post_hex="002028E06A48"
-  else
-    ui_print "- Only for Android 11, 10, Pie, Oreo and Nougat!"
+  elif [[ $API -lt 24 ]] ; then
+    ui_print "- Only for Android 7.0 (Nougat) and above"
     abort
   fi
-  echo -e "model=$model\nBOOTMODE=$BOOTMODE\nsys=$sys\nAPI=$API\nIS64BIT=$IS64BIT\nqcom=$qcom\nmod_path=$mod_path\nsys_path=$sys_path\npre_hex=$pre_hex\npost_hex=$post_hex" > $TMPDIR/vars.txt
+}
+
+search() {
+  ui_print "- Searching for lib path and relevant hex bytes sequence"
+  libpath=`find $sys/lib*|grep -E "\/(libbluetooth|bluetooth\.default)\.so$"|tail -n 1`
+  unzip -q $ZIPFILE hexpatch.sh -d $TMPDIR
+  chmod 755 $TMPDIR/hexpatch.sh
+  unzip -p $ZIPFILE bash.tar.xz|tar x -J -C $TMPDIR bash
+  chmod 755 $TMPDIR/bash
+  export TMPDIR API IS64BIT libpath
+  # Executed though bash for array handling
+  $TMPDIR/bash $TMPDIR/hexpatch.sh
 }
 
 extract() {
-  if [ ! -f $sys_path ] ; then
-    ui_print "- Aborting! Library not found!"
+  if [ ! -f $libpath ] ; then
+    ui_print "- Library not found!"
     abort
   else
     ui_print "- Copying library from system to module"
-    mkdir -p `echo $mod_path | xargs dirname`
-    cp -af $sys_path $mod_path
+    mod_path="$MODPATH/`echo $libpath|grep -o system.*`"
+    mkdir -p `dirname $mod_path`
+    cp -af $libpath $mod_path
   fi
 }
 
-patch_lib() {
+patchlib() {
   ui_print "- Applying patch"
-  if /data/adb/magisk/magiskboot hexpatch $mod_path $pre_hex $post_hex 2>/dev/null ; then
+  pre=`cat $TMPDIR/patch|sed -n '1 p'`
+  post=`cat $TMPDIR/patch|sed -n '2 p'`
+  if [[ $pre == $post ]] ; then
+    ui_print "- Library already (system-ly) patched!"
+    abort
+  elif /data/adb/magisk/magiskboot hexpatch $mod_path $pre $post ; then
     ui_print "- Successfully patched!"
   else
-    if /data/adb/magisk/magiskboot hexpatch $mod_path $post_hex 0 2>/dev/null ; then
-      ui_print "- Aborting! Library already (system-ly) patched!"
-    else
-      ui_print "- Aborting! Library not supported!"
-      cp -f $sys_path $sys/build.prop /data/misc/bluedroiddump/subBuffer.log $TMPDIR
-      cd $TMPDIR
-      tar -cf /sdcard/BluetoothLibPatcher-files.tar *
-      ui_print " "
-      ui_print "- To get support upload BluetoothLibPatcher-files.tar"
-      ui_print "  created in your internal storage to the XDA thread."
-    fi
-    rm -rf $MODPATH
+    ui_print "- Library not supported!"
+    echo -e "BOOTMODE=$BOOTMODE\nAPI=$API\nIS64BIT=$IS64BIT\nlibpath=$libpath" > $TMPDIR/debug
+    tar c -f /sdcard/BluetoothLibPatcher-files.tar $libpath $TMPDIR/patch $TMPDIR/debug
+    ui_print  " "
+    ui_print "- To get support upload BluetoothLibPatcher-files.tar"
+    ui_print 	"  created in your internal storage to the XDA thread"
     abort
   fi
 }
 
-set_vars
-
+check
+search
 extract
-
-patch_lib
+patchlib
---
+++ hexpatch.sh
@@ -0,0 +1,44 @@
+# BluetoothLibraryPatcher
+# hexpatch script
+# by 3arthur6
+
+bb=/data/adb/magisk/busybox
+qcom=`$bb grep -qw androidboot.hardware=qcom /proc/cmdline && echo 'true' || echo 'false'`
+
+# default(mostly arm64 exynos)=1   arm=2   qcom=3
+variant=`if ! $IS64BIT; then echo 2; elif $qcom; then echo 3; else echo 1; fi`
+
+hex=( \
+# default
+[130]=........f3031f2af4031f2a3e000014 [1130]=1f2003d5f3031f2af4031f2a3e000014 \
+[129]=........f4031f2af3031f2ae8030032 [1129]=1f2003d5f4031f2af3031f2ae8031f2a \
+[128]=88000034e803003248070035 [1128]=1f2003d5e8031f2a48070035 \
+[127]=88000034e803003228050035 [1127]=1f2003d5e8031f2a28050035 \
+[126]=${hex[127]} [1126]=${hex[1127]} \
+# arm
+[229]=..b100250120 [1229]=00bf00250020 \
+[228]=..b101200028 [1228]=00bf00200028 \
+[237]=09b1012032e0 [1227]=00bf002032e0 \
+[236]=08b1012031e0 [1226]=00bf002031e0 \
+[235]=087850bbb548 [1225]=0878bde0b548 \
+[234]=007840bb6a48 [1224]=002028e06a48 \
+# qcom
+[330]=88000054691180522925c81a6900003720008052 [1330]=04000014691180522925c81a69000037e0031f2a \
+[329]=88000054691180522925c81a69000037e0030032 [1329]=04000014691180522925c81a69000037e0031f2a \
+[328]=7f1d0071e91700f9e83c0054 [1328]=e0031f2ae91700f9e8010014)
+
+if ! $IS64BIT && [[ $API -ge 28 ]] ; then
+  hex[$variant$API]=`$bb xxd -p $libpath|$bb tr -d '\n'|$bb grep -om1 ${hex[$variant$API]}`
+elif $qcom && ! `$bb xxd -p $libpath|$bb tr -d '\n'|$bb grep -qm1 ${hex[$variant$API]}` ; then
+  variant='1'
+elif ! $qcom && [[ $API -ge 29 ]] ; then
+  hex[$variant$API]=`$bb xxd -p $libpath|$bb tr -d '\n'|$bb grep -om1 ${hex[$variant$API]}`
+fi
+if [[ -z ${hex[$variant$API]} ]] ; then
+  if `$bb xxd -p $libpath|$bb tr -d '\n'|$bb grep -qm1 ${hex[1$variant$API]}` ; then
+    hex[$variant$API]=${hex[1$variant$API]}
+  else
+    hex[$variant$API]=not_found
+  fi
+fi
+echo -e "${hex[$variant$API]}\n${hex[1$variant$API]}\n$($bb grep -o androidboot.bootloader=.* /proc/cmdline|$bb cut -d ' ' -f1|$bb cut -d '=' -f2)" > $TMPDIR/patch
