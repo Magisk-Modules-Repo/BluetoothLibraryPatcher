cc8153459d3cda923f302a45bd4acdef3db4d23f
Arthur Trouillot <37528999+3arthur6@users.noreply.github.com>
2020-12-30 12:28:02 +0100

    Whoops

--- service.sh
+++ service.sh
@@ -4,7 +4,7 @@
 
 previouslibmd5sum_tmp
 
-if [[ $previouslibmd5sum == `md5sum $(find $(magisk --path)/.magisk/mirror/system/lib*|grep -E "\/(libbluetooth|bluetooth\.default)\.so$"|tail -n 1)|cut -d " " -f1` ]] ; then
+if [[ $previouslibmd5sum != `md5sum $(find $(magisk --path)/.magisk/mirror/system/lib*|grep -E "\/(libbluetooth|bluetooth\.default)\.so$"|tail -n 1)|cut -d " " -f1` ]] ; then
   magisk --install-module /data/adb/modules/BluetoothLibraryPatcher/install.zip
 else
   exit
